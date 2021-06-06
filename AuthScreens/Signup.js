fa6186e7674ae9001f78710c01413df1af01d343
Rishabh Pathak <clusmsyknight@gmail.com>
2021-06-06 17:58:29 +0530

    added form fields in signup screen

--- AuthScreens/Signup.js
+++ AuthScreens/Signup.js
@@ -1,42 +1,128 @@
-import React from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
-
-import { Button, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  View,
+} from "react-native";
 import { Formik } from "formik";
+import { Input, Button, Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/core";
 
+/*
+    username: { type: String, unique: true, required: true },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+*/
 export const SignupForm = () => {
-  const onSubmit = () => {
-    console.log("Submit Form");
+  const initialValues = {
+    firstname: "",
+    lastname: "",
+    username: "",
+    email: "",
+    password: "",
   };
+
+  const onSubmit = (values) => {
+    console.log("Submit Form", values);
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <Formik initialValues={{ email: "" }} onSubmit={onSubmit}>
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
       {({ handleChange, handleBlur, handleSubmit, values }) => (
-        <View>
-          <TextInput
+        <KeyboardAvoidingView
+          behavior={"position"}
+          keyboardVerticalOffset={Platform.OS === "ios" ? 80 : 10}
+        >
+          <Input
+            onChangeText={handleChange("firstname")}
+            onBlur={handleBlur("firstname")}
+            placeholder="Enter your firstname:"
+            leftIcon={{ type: "feather", name: "user" }}
+            value={values.firstname}
+            autoCompleteType="name"
+            keyboardType="name-phone-pad"
+            textContentType="givenName"
+          />
+          <Input
+            onChangeText={handleChange("lastname")}
+            onBlur={handleBlur("lastname")}
+            placeholder="Enter your lastname:"
+            leftIcon={{ type: "feather", name: "user" }}
+            value={values.lastname}
+            autoCompleteType="name"
+            keyboardType="name-phone-pad"
+            textContentType="familyName"
+          />
+          <Input
+            onChangeText={handleChange("username")}
+            onBlur={handleBlur("username")}
+            placeholder="Enter your unique username:"
+            leftIcon={{ type: "feather", name: "user" }}
+            value={values.username}
+            autoCompleteType="username"
+            keyboardType="name-phone-pad"
+            textContentType="nickname"
+          />
+          <Input
             onChangeText={handleChange("email")}
             onBlur={handleBlur("email")}
+            placeholder="Enter your email:"
+            leftIcon={{ type: "feather", name: "mail" }}
             value={values.email}
+            autoCompleteType="email"
+            keyboardType="email-address"
+            textContentType="emailAddress"
+          />
+          <Input
+            onChangeText={handleChange("password")}
+            onBlur={handleBlur("password")}
+            placeholder="Enter your password:"
+            leftIcon={{ type: "feather", name: "lock" }}
+            value={values.password}
+            autoCompleteType="password"
+            secureTextEntry={!showPassword}
+            textContentType="password"
+            rightIcon={
+              <Icon
+                type="feather"
+                name={showPassword ? "eye-off" : "eye"}
+                onPress={() => setShowPassword(!showPassword)}
+              />
+            }
           />
           <Button onPress={handleSubmit} title="Submit" />
-        </View>
+        </KeyboardAvoidingView>
       )}
     </Formik>
   );
 };
 
 export default function Signup() {
+  const navigation = useNavigation();
+  const goToLoginScreen = () => navigation.navigate("Login");
+
   return (
     <SafeAreaView style={styles.container}>
       <SignupForm />
+      <View>
+        <Button
+          title="Already have an account?"
+          onPress={goToLoginScreen}
+          type="clear"
+        />
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 20,
   },
 });
