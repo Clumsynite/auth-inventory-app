aa99074d56bf0c0352a911e215c3142032757809
Rishabh Pathak <clusmsyknight@gmail.com>
2021-06-12 23:26:06 +0530

    added login logic

--- AuthScreens/Login.js
+++ AuthScreens/Login.js
@@ -1,82 +1,123 @@
 import { useNavigation } from "@react-navigation/core";
 import React, { useState } from "react";
-import { SafeAreaView, StyleSheet, View, Text, ScrollView } from "react-native";
+import { SafeAreaView, StyleSheet, View, ScrollView } from "react-native";
 import { Formik } from "formik";
 import { Input, Button, Icon } from "react-native-elements";
-import { string } from "prop-types";
+
+import { ErrorMessage, Snackbar, TranslucentLoader } from "../components";
 
 import LoginSchema from "../models/LoginSchema";
+import { login } from "../api/auth";
 
 export const LoginForm = () => {
   const initialValues = {
     username: "",
     password: "",
   };
+  const [showPassword, setShowPassword] = useState(false);
+  const [submitting, setSubmitting] = useState(false);
+  const [snackbar, setSnackbar] = useState();
 
-  const onSubmit = (values) => {
-    console.log("Submit Form", values);
+  const dismissSnackbar = () => setSnackbar(false);
+
+  const onSubmit = async (values, { resetForm }) => {
+    try {
+      setSubmitting(true);
+      const data = await login(values);
+      console.log("data", data);
+      setSubmitting(false);
+      if (data.success) {
+        resetForm();
+        setSnackbar({
+          msg: data.msg,
+          className: "success",
+          accent: "red",
+        });
+      } else if (data.message && data.name) {
+        setSnackbar({
+          msg: data.message,
+          className: "error",
+          accent: "white",
+        });
+      } else if (!data.success) {
+        setSnackbar({
+          msg: data.error,
+          className: "error",
+          accent: "white",
+        });
+      }
+    } catch (e) {
+      setSubmitting(false);
+      console.error(e);
+    }
   };
 
-  const [showPassword, setShowPassword] = useState(false);
-
-  const ErrorMessage = ({ error }) => (
-    <Text style={{ fontSize: 14, color: "#F00" }}>{error}</Text>
-  );
-  ErrorMessage.propTypes = { error: string.isRequired };
   return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={onSubmit}
-      validationSchema={LoginSchema}
-    >
-      {({
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        values,
-        errors,
-        touched,
-      }) => (
-        <View>
-          {errors.username && touched.username ? (
-            <ErrorMessage error={errors.username} />
-          ) : null}
-          <Input
-            onChangeText={handleChange("username")}
-            onBlur={handleBlur("username")}
-            placeholder="Enter a unique username:"
-            leftIcon={{ type: "feather", name: "user" }}
-            value={values.username}
-            autoCompleteType="username"
-            keyboardType="name-phone-pad"
-            textContentType="nickname"
-            returnKeyType="next"
-          />
-          {errors.password && touched.password ? (
-            <ErrorMessage error={errors.password} />
-          ) : null}
-          <Input
-            onChangeText={handleChange("password")}
-            onBlur={handleBlur("password")}
-            placeholder="Enter your password:"
-            leftIcon={{ type: "feather", name: "lock" }}
-            value={values.password}
-            autoCompleteType="password"
-            secureTextEntry={!showPassword}
-            textContentType="password"
-            rightIcon={
-              <Icon
-                type="feather"
-                name={showPassword ? "eye-off" : "eye"}
-                onPress={() => setShowPassword(!showPassword)}
-              />
-            }
-            returnKeyType="done"
-          />
-          <Button onPress={handleSubmit} title="Submit" />
-        </View>
+    <>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={LoginSchema}
+      >
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          errors,
+          touched,
+        }) => (
+          <View>
+            {errors.username && touched.username ? (
+              <ErrorMessage error={errors.username} />
+            ) : null}
+            <Input
+              onChangeText={handleChange("username")}
+              onBlur={handleBlur("username")}
+              placeholder="Enter a unique username:"
+              leftIcon={{ type: "feather", name: "user" }}
+              value={values.username}
+              autoCompleteType="username"
+              keyboardType="name-phone-pad"
+              textContentType="nickname"
+              returnKeyType="next"
+            />
+            {errors.password && touched.password ? (
+              <ErrorMessage error={errors.password} />
+            ) : null}
+            <Input
+              onChangeText={handleChange("password")}
+              onBlur={handleBlur("password")}
+              placeholder="Enter your password:"
+              leftIcon={{ type: "feather", name: "lock" }}
+              value={values.password}
+              autoCompleteType="password"
+              secureTextEntry={!showPassword}
+              textContentType="password"
+              rightIcon={
+                <Icon
+                  type="feather"
+                  name={showPassword ? "eye-off" : "eye"}
+                  onPress={() => setShowPassword(!showPassword)}
+                />
+              }
+              returnKeyType="done"
+            />
+            <Button onPress={handleSubmit} title="Submit" />
+          </View>
+        )}
+      </Formik>
+      {submitting && <TranslucentLoader visible={submitting} />}
+      {snackbar && (
+        <Snackbar
+          visible={!!snackbar}
+          message={snackbar.msg}
+          className={snackbar.className}
+          accent={snackbar.accent}
+          dismiss={dismissSnackbar}
+        />
       )}
-    </Formik>
+    </>
   );
 };
 
