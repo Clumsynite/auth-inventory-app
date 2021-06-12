d964e73fc1913bdb9cc5a4117900b3e92be67578
Rishabh Pathak <clusmsyknight@gmail.com>
2021-06-12 21:13:52 +0530

    created an index file for components

---
+++ components/index.js
@@ -0,0 +1,7 @@
+import ErrorMessage from "./ErrorMessage";
+import ImagePicker from "./ImagePicker";
+import Snackbar from "./Snackbar";
+import Toast from "./Toast";
+import TranslucentLoader from "./TranslucentLoader";
+
+export { ErrorMessage, ImagePicker, Snackbar, Toast, TranslucentLoader };
