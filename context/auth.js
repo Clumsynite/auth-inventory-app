6bff3ce69b2a8ab784f2a5f61eb3b79918b5aee3
Rishabh Pathak <clusmsyknight@gmail.com>
2021-06-13 14:59:46 +0530

    fixed context

--- context/auth.js
+++ context/auth.js
@@ -1,21 +1,22 @@
 import React, { createContext, useReducer, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-let initialState = null;
+let initialState = { user: null, token: null };
 
 const AuthContext = createContext({
-  user: null,
-  login: (user) => user,
+  state: initialState,
+  login: ({ user, token }) => {
+    user, token;
+  },
   logout: () => null,
 });
 
-async function AuthReducer(state, action) {
+function AuthReducer(state, action) {
   switch (action.type) {
     case "LOGIN":
-      await storeData("user", action.payload);
       return action.payload;
     case "LOGOUT":
-      return null;
+      return initialState;
     default:
       return state;
   }
@@ -24,6 +25,22 @@ async function AuthReducer(state, action) {
 const storeData = async (key, value) => {
   try {
     await AsyncStorage.setItem(key, value);
+  } catch (e) {
+    console.error("Error storing data", e);
+  }
+};
+
+const getData = async (key) => {
+  try {
+    return await AsyncStorage.getItem(key);
+  } catch (e) {
+    console.error("Error reading value", e);
+  }
+};
+
+const storeObjectData = async (key, value) => {
+  try {
+    return await AsyncStorage.setItem(key, JSON.stringify(value));
   } catch (e) {
     console.error("Error storing data", e);
   }
@@ -38,6 +55,14 @@ const getObjectData = async (key) => {
   }
 };
 
+const removeItem = async (key) => {
+  try {
+    await AsyncStorage.removeItem(key);
+  } catch (e) {
+    console.error("Error removing value", e);
+  }
+};
+
 function AuthProvider(props) {
   const [state, dispatch] = useReducer(AuthReducer, initialState);
 
@@ -48,13 +73,34 @@ function AuthProvider(props) {
   const init = async () => {
     try {
       const user = await getObjectData("user");
-      dispatch("LOGIN", user);
+      const token = await getData("token");
+      dispatch({ type: "LOGIN", payload: { user, token } });
     } catch (error) {
       console.error("Error initialising user", error);
     }
   };
 
-  return <AuthContext.Provider value={{ user: state }} {...props} />;
+  const login = async ({ user, token }) => {
+    try {
+      await storeObjectData("user", user);
+      await storeData("token", token);
+      dispatch({ type: "LOGIN", payload: { user, token } });
+    } catch (e) {
+      console.error("Error login ", e);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await removeItem("user");
+      await removeItem("token");
+      dispatch({ type: "LOGOUT" });
+    } catch (e) {
+      console.error("Error login ", e);
+    }
+  };
+
+  return <AuthContext.Provider value={{ state, login, logout }} {...props} />;
 }
 
 export { AuthContext, AuthProvider };
