12142d466f86642a4bde582239bb78a214c8c530
Rishabh Pathak <clusmsyknight@gmail.com>
2021-06-13 17:27:21 +0530

    added api calls for inventory

---
+++ api/inventory.js
@@ -0,0 +1,86 @@
+import { url } from ".";
+
+export const getItems = async (token) => {
+  try {
+    const response = await fetch(`${url}/inventory`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${token}`,
+      },
+      credentials: "include",
+    });
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getItem = async (id, token) => {
+  try {
+    const response = await fetch(`${url}/inventory/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${token}`,
+      },
+      credentials: "include",
+    });
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const addItem = async (item, token) => {
+  try {
+    const response = await fetch(`${url}/user`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${token}`,
+      },
+      credentials: "include",
+      body: JSON.stringify(item),
+    });
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const updateItem = async (item, token) => {
+  try {
+    const response = await fetch(`${url}/user`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${token}`,
+      },
+      credentials: "include",
+      body: JSON.stringify(item),
+    });
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const deleteItem = async (item, token) => {
+  try {
+    const response = await fetch(`${url}/user`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${token}`,
+      },
+      credentials: "include",
+      body: JSON.stringify(item),
+    });
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default { getItems, getItem, addItem, updateItem, deleteItem };
