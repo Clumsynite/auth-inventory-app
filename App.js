9a07011b1f49ba2004f09b73540946169af3e9b6
Rishabh Pathak <clusmsyknight@gmail.com>
2021-06-15 22:57:55 +0530

    wrapped with react-native-paper Provider
    for FAB group in Inventory screen

--- App.js
+++ App.js
@@ -2,16 +2,19 @@
 /* eslint-disable no-undef */
 import React from "react";
 import { MenuProvider } from "react-native-popup-menu";
+import { Provider as PaperProvider } from "react-native-paper";
 
 import { AuthProvider } from "./context/auth";
 import Navigation from "./Navigation";
 
 export default function App() {
   return (
-    <AuthProvider>
-      <MenuProvider>
-        <Navigation />
-      </MenuProvider>
-    </AuthProvider>
+    <PaperProvider>
+      <AuthProvider>
+        <MenuProvider>
+          <Navigation />
+        </MenuProvider>
+      </AuthProvider>
+    </PaperProvider>
   );
 }
