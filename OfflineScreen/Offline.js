d4e767715af8aacd7f3292c7bce7fd2e56d7914a
Rishabh Pathak <clusmsyknight@gmail.com>
2021-06-20 19:46:37 +0530

    refactored

--- OfflineScreen/Offline.js
+++ OfflineScreen/Offline.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { SafeAreaView, StyleSheet, View, Text } from "react-native";
 
-export default function Login() {
+export default function OfflineScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <View>
